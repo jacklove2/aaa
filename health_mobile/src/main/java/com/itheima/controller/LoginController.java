f759fe55dbf7937a799cd60938c4e809a47ff572
javklove2 <tx_25910@163.com>
2019-12-23 15:49:56 +0800

    最后一行加注释

--- health_mobile/src/main/java/com/itheima/controller/LoginController.java
+++ health_mobile/src/main/java/com/itheima/controller/LoginController.java
@@ -67,5 +67,6 @@ public class LoginController {
         cookie.setPath("/");// 只要用户访问了网站
         res.addCookie(cookie); // 只要用户访问了网站，就会在请求头的Header中带上这个cookie参数
         return new Result(true, MessageConstant.LOGIN_SUCCESS);
+        //测试aaa
     }
 }
